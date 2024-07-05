8fa125c524baa36ee9c21a79d38792a54eaa74c5
YiQun0422 <77156038+YiQun0422@users.noreply.github.com>
2024-07-05 13:42:18 +0800

    Update list.c

--- list.c
+++ list.c
@@ -3,69 +3,52 @@
 #include <stdbool.h>
 #include <malloc.h>
 
-#if 0 // 使用数组（线性结构）实现栈
+#if 1 // 顺序栈的表示  栈非空时 top永远指向栈顶元素的上一个位置
 typedef struct {
-    int* base;
-    int* top;
-    int stacksize;
+    int* base;  //栈底指针
+    int* top;   //栈顶指针
+    int stacksize;  //栈可用的最大容量
 } Stack;
 
 // 创建一个栈
 // 参数 max_size: 支持的最大长度
 Stack* StackCreate(int max_size) {
-    Stack *s=malloc(sizeof(Stack));
-    s->base = malloc(sizeof(int)*max_size);
-    s->top = s->base;
-    s->stacksize = max_size;
+    Stack* s;
+    s->base = malloc(sizeof(int)*max_size);  //为顺序栈动态分配最大容量为max_size的数组空间
+    s->top = s->base;  //top初始为base 空栈
+    s->stacksize = max_size;  //栈最大容量为max_size
 
     return s;
 }
 
-// 删除 StackCreate 创建的栈
-void StackDestory(Stack* stack) {
-    if (StackEmpty(stack))
-        return 0;
-    stack->top--;
-}
-
-// 判断栈是否为空, 返回1表示空, 0表示不空 (int强制转换为bool, 0为false, 其他为true)
-int StackEmpty(Stack* stack) {
-    if (stack->top == stack->base)
-        return 1;
-    return 0;
-}
-
-// 判断栈是否已满, 返回1表示满，0表示不满
-int StackFull(Stack* stack) {
-    if (stack->top - stack->base == stack->stacksize)
-        return 1;
-    return 0;
-}
-
-// 向栈顶放入一个元素
+// 顺序栈的入栈
 void StackPush(Stack* stack, int value) {
-    if (!StackFull(stack))
-        *stack->top = value;
-        stack->top++;
+    if (stack->top - stack->base == stack->stacksize)
+        return false; //栈满 指向数组元素下一个空间
+    *stack->top = value;
+    stack->top++;  //元素value压入栈顶 栈顶指针加1
+}
+
+// 从栈顶取出一个元素 用value返回其值
+void StackPop(Stack* stack, int* value) {
+    if (stack->top == stack->base)
+        return false; //栈空
+    stack->top--;
+    value = *stack->top;  //栈顶指针减1 将栈顶元素赋给value
 }
 
 // 获取栈顶的元素（不取出）
 int StackTop(Stack* stack) {
     if (stack->top != stack->base)
-        return *(stack->top - 1);
-}
-
-// 从栈顶取出一个元素 用value返回其值
-void StackPop(Stack* stack,int* value) {
-    if (!StackEmpty(stack))
-        stack->top--;
-        value=stack->top;
+        return *(stack->top - 1);  //返回栈顶元素的值
 }
 
 #endif
 
 // 由于这里的定义和上面的重复了, 所以可以把上面的 #if 1 改为 0 , 这里的改为 1 就可以切换不同实现
-#if 1 // 使用链表（链式结构）实现栈 后进先出
+#if 0 // 使用链表（链式结构）实现栈 后进先出
+//  区别于顺序结构, 链式结构支持无限长度, 所以没有 max_size 也不需要 StackFull 函数
+
 typedef struct {
     int data;
     struct Stack* next; //和单链表一致
@@ -105,31 +88,49 @@ int StackTop(Stack* stack)
 
 #endif
 
-#if 0 // 使用数组（线性结构）实现队列
+#if 1 // 队列的顺序表示 循环队列
 typedef struct {
-
+    int* base; //存储空间的基地址
+    int front; //头指针
+    int rear;  //尾指针
 } Queue;
 
-Queue* QueueCreate(int max_size);
+Queue* QueueCreate(int max_size) {
+    Queue* q;
+    q->base = malloc(sizeof(int) * max_size); //为队列分配数组空间
+    q->front = q->rear = q; //队列为空
+}
 
-void QueueDestory(Queue* queue);
+//求队列长度
+int QueueLength(Queue* queue,int max_size) {
+    return(queue->rear - queue->front + max_size) % max_size;
+}
 
-int QueueEmpty(Queue* queue);
+// 循环队列的入队
+void QueuePushback(Queue* queue, int value,int max_size) {
+    if ((queue->rear + 1) % max_size == queue->front)
+        return false; //队满
+    queue->base[queue->rear] = value;  //新元素插入队尾
+    queue->rear = (queue->rear + 1) % max_size;  //队尾指针加1
+}
 
-int QueueFull(Queue* queue);
-
-// 把元素放入队列尾部
-void QueuePushback(Queue* queue, int value);
+// 移出队头元素 用value返回其值
+void QueuePopfront(Queue* queue,int* value,int max_size) {
+    if (queue->front == queue->rear)
+        return false; //队空
+    value = queue->base[queue->front]; //保存队头元素
+    queue->front = (queue->front + 1) % max_size; //队头指针加1
+}
 
 // 获取队头元素
-int QueueFront(Queue* queue);
-
-// 移出队头元素
-void QueuePopfront(Queue* queue);
+int QueueFront(Queue* queue) {
+    if (queue->front != queue->rear)
+        return queue->base[queue->front];  //返回队头元素的值
+}
 
 #endif
 
-#if 1 // 使用链表（链式结构）实现队列 先进先出
+#if 0 // 使用链表（链式结构）实现队列 先进先出
 typedef struct
 {
     int data;
@@ -147,6 +148,8 @@ Queue* QueueCreate() {
     q->front = malloc(sizeof(QNode));//新生成的结点作为头节点
     q->front->next = NULL;//带头结点,头结点指针域置空
     q->rear = q->front;//队尾指向队指向新节点
+
+    return q;
 }
 
 // 链队的入队
